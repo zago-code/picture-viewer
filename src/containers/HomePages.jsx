2dd5f3a0f78be007bd0a5d972d02ca884f7b9b20
Gabriel Zarate <gzaratet1@gmail.com>
2020-09-07 11:11:12 -0500

    fix Hook 2

--- src/containers/HomePages.jsx
+++ src/containers/HomePages.jsx
@@ -4,27 +4,24 @@ import Button from '../components/Button';
 import '../assets/style/containers/HomePages.css';
 
 const HomePages = () => {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState({ favorites: [], animals: [] });
   useEffect(() => {
     fetch('http://localhost:3000/initialState')
       .then((response) => response.json())
       .then((data) => setImages(data))
       .catch((error) => console.error(error));
   }, []);
-  // const { id } = images.animals[0].id;
-  // const getSource = () => {
-  //   const animal = images.animals.length;
-  //   return animal;
-  // };
-  console.log(images.animals);
+  const getSourceForIndex = (index) => ({ ...images.animals[index] });
+  console.log(getSourceForIndex(1));
+  const { source, title } = getSourceForIndex(7);
   return (
     <>
       <Header />
       <section className='viewer__container'>
         <figure className='viewer__image'>
           <img
-            src='/'
-            alt='foto-animal'
+            src={source}
+            alt={`ìmagen-${title}`}
           />
         </figure>
         <div className='button__container'>
